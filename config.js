bb3d8d841c0f1246ef16be1d1bf342c978bc4e75
minhphatcode <nminhphat2024@gmail.com>
2025-01-22 22:44:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Mai Trúc Ly';
+const giftUrl = '.';
+const eventName = 'Chúc Mừng Năm Mới 2025';
+const titleCard = 'Gửi Mai Trúc Ly💝💌';
+const contentCard = '"Dịp năm mới, Phát chúc cho Ly và gia đình luôn mạnh khỏe, hạnh phúc, và tràn ngập niềm vui. Mong mọi điều tốt đẹp nhất sẽ đến với Ly và những người thân yêu. Chúc năm mới của Ly thật thành công và trọn vẹn!"💘';
+
+const giftImage = 'd:\z6254290710816_76069bf83e0588787aff5ed8e55a16b0.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
